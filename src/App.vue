3c20c1e402580c5f08efe4b5851088cf61181813
Lord Sayur <umar_317@hotmail.com>
2020-10-04 20:50:08 +0800

    Add p5 background component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div>
     <router-view/>
   </div>
 </template>
@@ -13,11 +9,11 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body{
+  margin: 0;
 }
-
-#nav {
+.p5Canvas {
+  width: 100% !important;
   padding: 30px;
 
   a {
---
+++ src/components/P5Background.vue
@@ -0,0 +1,36 @@
+<template>
+  <vueP5 v-on="{preload, setup, draw, keypressed}" />
+</template>
+
+<script>
+import VueP5 from "vue-p5";
+
+export default {
+  name: 'P5Background',
+  components: { VueP5 },
+
+  data() {
+    return {
+      p5: null,
+      x: window.innerWidth,
+      y: window.innerHeight + 100,
+    };
+  },
+
+  methods: {
+    preload(p5) {
+      this.p5 = p5;
+    },
+    setup() {
+      this.p5.createCanvas(this.x, this.y);
+      this.p5.background(0);
+    },
+    draw() {
+    },
+    keypressed(data) {},
+  },
+}
+</script>
+
+<style scoped lang="scss">
+</style>
