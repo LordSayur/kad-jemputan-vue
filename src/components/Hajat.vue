482a0f4e7506f0574c0b4fb4890fa50c70984e1b
Lord Sayur <umar_317@hotmail.com>
2020-11-25 07:00:43 +0800

    add hajat component

---
+++ src/components/Hajat.vue
@@ -0,0 +1,33 @@
+<template>
+  <section id="hajat" class="center-align">
+    <span class="jawi">بِسْمِ اٌللَّهِ اٌلرَّحْمٰنِ اٌلرَحِيمِ</span>
+    <span class="jawi">يا قاضى الحاجاة</span>
+    <ul>
+      <li>Dr. Haji Ibrahim Bin Haji Abdul Rahman
+      </li>
+      <li>Hajah Siti Fatimah Binti Haji Mohd. Salleh</li>
+      <li>Maidin bin Lampoh</li>
+      <li>Hamizah binti Abdullah @ corpin</li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Hajat",
+}
+</script>
+
+<style lang='scss'>
+@import '@/main.scss';
+#hajat{
+  margin: 2rem auto;
+  span{
+    display: block;
+  }
+  .jawi {
+    font-family: $qadhiFont;
+    font-size: 1.5rem;
+  }
+}
+</style>
