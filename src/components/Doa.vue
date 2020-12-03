fef00f7e404dae9b1886bbc87128a67e21a0937b
Lord Sayur <umar_317@hotmail.com>
2020-12-03 12:36:11 +0800

    add doa component

---
+++ src/components/Doa.vue
@@ -0,0 +1,30 @@
+<template>
+  <section v-if="false" id="doa" class="row center-align hide-behind">
+    <span>Doa Pengantin</span>
+    <p>
+      Ya Allah Ya Tuhan kami <br>
+      Satukanlah hati kedua mempelai ini
+    </p>
+    <div>
+      <span>Mohammad Omar bin Dr. Haji Ibrahim</span>
+      <span>dengan</span>
+      <span>Nurul Amirah binti Maidin</span>
+    </div>
+    <p>
+      seperti Engkau satukan Nabi Adam dan Hawa seperti Engkau satukan Nabi Yusof dan Zulaika dan seperti Engkau satukan Nabi Muhammad S.A.W. dan Siti Khadijah
+    </p>
+    <p>Ya Allah</p>
+    <p>Berkatilah majlis yang diadakan, jadikanlah pasangan ini yang berbahagia dan berkekalan hingga ke ahir hayat dan kurniakanlah kepada mereka zuriat yang sempurna, beriman dan beramal soleh, sesungguhnya Engkau maha berkuasa atas segala sesuatu.</p>
+    <p>Amin Ya Rabbal Alamin</p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Doa'
+}
+</script>
+
+<style>
+
+</style>
