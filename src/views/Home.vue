96b5ddf2c364f425ce30bca0c829831518aeb5a6
Lord Sayur <umar_317@hotmail.com>
2020-12-05 17:51:45 +0800

    embed wsb

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,7 +20,10 @@
       <div v-html='content' style="margin-bottom: 3rem;"></div>
     
     <Address class='animate__animated animate__fadeInUp animate__delay-4s' :side='side'/>
+    </div>
     
+    <h6 style="text-align: center;text-decoration: underline;margin-bottom: 1rem;"><a href="https://wsb.netlify.app/">Waktu Sembahyang Brunei</a></h6>
+    <iframe src="https://wsb.netlify.app/" style="height: 35rem; width: 100%" frameborder="0"></iframe>
   </div>
 </template>
 
