327a92f1bb8d0c0a09ef51bb6df8ee322afe570a
Lord Sayur <umar_317@hotmail.com>
2020-12-05 17:52:04 +0800

    comment p5

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,35 @@
 <template>
-  <div class="home">
-    <!-- <P5Background/> -->
-
-    <GroomAndBride class="animate__animated animate__fadeIn animate__delay-2s"/>
-
-    <Jemputan class='animate__animated animate__fadeInUp animate__delay-3s' :nama='nama' :kampong='kampong' :side='side'/>
-
-    <section v-if="geng != ''" id="customMessage" class="row center-align">
-      <p>
-        {{ customMessage }}
-      </p>
-    </section>
+  <div>
+    <div class="home">
+      <!-- <P5Background/> -->
     
-    <Majlis class='animate__animated animate__fadeInUp animate__delay-4s' :side='side'/>
-
-    <Tentative class='animate__animated animate__fadeInUp animate__delay-4s' :side="side"/>
-
+      <GroomAndBride class="animate__animated animate__fadeIn animate__delay-2s"/>
+    
+      <Jemputan class='animate__animated animate__fadeInUp animate__delay-3s' :nama='nama' :kampong='kampong' :side='side'/>
+    
+      <section v-if="geng != ''" id="customMessage" class="row center-align">
+        <p>
+          {{ customMessage }}
+        </p>
+      </section>
+      
+      <Majlis class='animate__animated animate__fadeInUp animate__delay-4s' :side='side'/>
+    
+      <Tentative class='animate__animated animate__fadeInUp animate__delay-4s' :side="side"/>
+    
       <h5>{{ title }}</h5>
       <div v-html='content' style="margin-bottom: 3rem;"></div>
     
-    <Address class='animate__animated animate__fadeInUp animate__delay-4s' :side='side'/>
+      <Address class='animate__animated animate__fadeInUp animate__delay-4s' :side='side'/>
     </div>
-    
+
     <h6 style="text-align: center;text-decoration: underline;margin-bottom: 1rem;"><a href="https://wsb.netlify.app/">Waktu Sembahyang Brunei</a></h6>
     <iframe src="https://wsb.netlify.app/" style="height: 35rem; width: 100%" frameborder="0"></iframe>
   </div>
 </template>
 
 <script>
-import P5Background from '@/components/P5Background.vue'
+// import P5Background from '@/components/P5Background.vue'
 import GroomAndBride from '@/components/GroomAndBride.vue'
 import Jemputan from '@/components/Jemputan.vue'
 import Majlis from '@/components/Hajat.vue'
@@ -40,7 +41,7 @@ const fb = require('@/firebaseConfig.js')
 export default {
   name: 'Home',
   components: {
-    P5Background,
+    // P5Background,
     GroomAndBride,
     Jemputan,
     Majlis,
