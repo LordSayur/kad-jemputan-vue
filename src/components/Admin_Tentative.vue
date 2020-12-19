ae9a6377d750a997ee3662ce0271a1f65efa229b
Lord Sayur <umar_317@hotmail.com>
2020-12-19 11:49:08 +0800

    Update Admin Tentative layout

--- src/components/Admin_Tentative.vue
+++ src/components/Admin_Tentative.vue
@@ -3,33 +3,45 @@
     <p v-if="!agendas">Fetching data...</p>
     <form v-else @submit.prevent="updateDb" class='col s12'>
       <h2>Tentative - {{ isOmar ? 'Omar' : 'Amirah'}}</h2>
+      <!-- Buttons -->
       <div class="row">
           <button type="submit" :class="`btn`">Update to Database</button>
           <UpdateDb v-if="states.isShow" docName='agendas' :payload="{agendas}" @updateDone="states.isShow = false" />
       </div>
-      <div v-for="(agenda, index) in Agendas" :key="index">
-        <div class="row">
-          <div class="col s12">
-            <textarea class="materialize-textarea" v-model="agenda.agenda"></textarea>
+      <!-- List -->
+      <div v-for="(agenda, index) in Agendas" :key="index" class="row">
+        <div class="tentative">
+          <div>
+            <label :for="'agenda'+index">Agenda</label>
+            <textarea :id="'agenda'+index" v-model="agenda.agenda"></textarea>
           </div>
-          <div  class="col s12 myFlex">
-            <input v-model="agenda.time" type="text">
+          <div class="tentative-inner">
+            <div>
+              <label :for="'masa'+index">Masa</label>
+              <input :id="'masa'+index" v-model="agenda.time" type="text">
+            </div>
             <select v-model="agenda.period" style="display: block">
               <option value="" disabled selected>Choose Status</option>
               <option value="am">am</option>
               <option value="pm">pm</option>
             </select>
-            <select v-model="agenda.status" style="display: block">
-              <option value="" disabled selected>Choose Status</option>
-              <option value="todo">Todo</option>
-              <option value="currently">Current</option>
-              <option value="done">Done</option>
-            </select>
+          </div>
+          <div class="tentative-inner">
+            <div>
+              <label :for="'status'+index">Status</label>
+              <select :id="'status'+index" v-model="agenda.status" style="display: block">
+                <option value="" disabled selected>Choose Status</option>
+                <option value="todo">Todo</option>
+                <option value="currently">Current</option>
+                <option value="done">Done</option>
+              </select>
+            </div>
             <button :class="`btn red`" @click="deleteAgenda(index)">X</button>
           </div>
-          <div class="teal" style="height: 3px"></div>
         </div>
+        <div class="teal" style="height: 3px"></div>
       </div>
+      <!-- Buttons -->
       <div class="row">
         <button type="button" :class="`btn`" @click="addAgenda()">Add New</button>
       </div>
@@ -95,6 +107,17 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang='scss' scoped>
+.tentative {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+}
+.tentative-inner {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
 </style>
